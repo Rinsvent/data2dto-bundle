97253a65840f8a90511b00e0edc678a1e6d5e1f1
Rinsvent <rinsvent007@gmail.com>
2021-08-14 18:19:37 +0700

    Поправил extansion

--- src/DependencyInjection/Data2DTOExtension.php
+++ src/DependencyInjection/Data2DTOExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class RequestExtension extends Extension
+class Data2DTOExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
